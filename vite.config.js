9bded7086240634381edda35f2ceb2b8602e433e
Leon-Carlo <carloraul826@gmail.com>
2025-03-19 12:53:46 -0500

    actualizacion

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        // Cambia la salida de la construcción a 'public/build'
+        outDir: 'public/build',  // Coloca los archivos de salida en public/build
+        emptyOutDir: true,  // Esto asegura que la carpeta 'public/build' se limpie antes de generar nuevos archivos
+    },
 });
